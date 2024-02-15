22468639fce81dd6316b61d069813b7b83f4a8ab
MaelMbange <maelmbange21@gmail.com>
2024-02-15 22:27:47 +0100

    Added classes

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,6 @@
 import 'package:halo_api_commander/functions.dart';
 
 void main() async {
-  //startDownloadImages();
-  startConcatenateImages(start: 5, end: 12);
+  startDownloadImages();
+  //startConcatenateImages(start: 5, end: 12);
 }
